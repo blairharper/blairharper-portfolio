7d21a794c16063b708c1a418ddf98d0f140a6dc8
Blair Harper <blair@passio.co.uk>
2019-06-28 21:46:13 +0100

    initial commit

---
+++ src/components/home/home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="home" id="home-content">
+      <h1>Blair Harper</h1>
+      <p>Software Engineer | Edinburgh | blair@passio.co.uk</p>
+      <p>
+        I work for <a href="https://passio.co.uk">Passio</a>, a digital agency.
+      </p>
+      <p>
+        We build accessible websites and apps for people with "disabilities",
+        cognitive impairments, & neurological differences.
+      </p>
+    </div>
+  );
+}
